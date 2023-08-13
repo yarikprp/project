5dcc5443c2069fc9cdde4a9d15d3cb2fef303a99
yarikprp <yaroslav.popov.03@gmail.com>
2023-08-13 15:46:01 +0300

    редактирование объявлений

--- myapp/src/store/ads.js
+++ myapp/src/store/ads.js
@@ -21,6 +21,14 @@ export default {
     },
     loadAds (state, payload) {
       state.ads = payload
+    },
+    updateAd (state, {title, description, id}) {
+      const ad = state.ads.find(a => {
+        return a.id === id
+      })
+
+      ad.title = title
+      ad.description = description
     }
   },
   actions: {
@@ -85,6 +93,24 @@ export default {
         commit('setLoading', false)
         throw error
       }
+    },
+    async updateAd ({commit}, {title, description, id}) {
+      commit('clearError')
+      commit('setLoading', true)
+
+      try {
+        await fb.database().ref('ads').child(id).update({
+          title, description
+        })
+        commit('updateAd', {
+          title, description, id
+        })
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
     }
   },
   getters: {
@@ -96,8 +122,10 @@ export default {
         return ad.promo
       })
     },
-    myAds (state) {
-      return state.ads
+    myAds (state, getters) {
+      return state.ads.filter(ad => {
+        return ad.ownerId === getters.user.id
+      })
     },
     adById (state) {
       return adId => {
