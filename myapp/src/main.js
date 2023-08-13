be05d4d34148ceff91fad46df6e4d61285df1a76
yarikprp <yaroslav.popov.03@gmail.com>
2023-08-13 16:49:46 +0300

    поменял цвет

--- myapp/src/main.js
+++ myapp/src/main.js
@@ -7,6 +7,17 @@ import BuyModalComponent from '@/components/Shared/BuyModal'
 import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#f44336',
+    secondary: '#9575CD',
+    accent: '#9c27b0',
+    error: '#f44336',
+    warning: '#3949AB',
+    info: '#2196f3',
+    success: '#4caf50'
+  }
+})
 Vue.use(Vuetify)
 Vue.component('app-buy-modal', BuyModalComponent)
 Vue.config.productionTip = false
