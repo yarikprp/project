0480629250e3a79a07d39156b506816f2aed73e6
yarikprp <yaroslav.popov.03@gmail.com>
2023-08-13 16:38:46 +0300

    ю

--- myapp/src/components/User/Orders.vue
+++ myapp/src/components/User/Orders.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
+      <v-flex xs12 class="text-xs-center pt-5" v-if="loading">
+        <v-progress-circular
+          indeterminate
+          :size="100"
+          :width="4"
+          color="purple"
+        ></v-progress-circular>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 v-else-if="!loading && orders.length !== 0">
         <h1 class="text--secondary mb-3">Orders</h1>
         <v-list two-line subheader>
           <v-list-tile
@@ -29,29 +37,34 @@
           </v-list-tile>
         </v-list>
       </v-flex>
+      <v-flex xs12 class="text-xs-center" v-else>
+        <h1 class="text--secondary">You have no orders</h1>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        orders: [
-          {
-            id: 'fds3',
-            name: 'Vladilen',
-            phone: '8-921-121-12-12',
-            adId: '123',
-            done: false
-          }
-        ]
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      },
+      orders () {
+        return this.$store.getters.orders
       }
     },
     methods: {
       markDone (order) {
-        order.done = true
+        this.$store.dispatch('markOrderDone', order.id)
+          .then(() => {
+            order.done = true
+          })
+          .catch(() => {})
       }
+    },
+    created () {
+      this.$store.dispatch('fetchOrders')
     }
   }
 </script>
